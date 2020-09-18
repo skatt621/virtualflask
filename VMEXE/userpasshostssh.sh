f1c48167addb7a4554b0dcca1f5507d41d938e96
clouduser <clouduser@notrealemail.com>
2020-09-18 17:39:41 -0400

    what if they want to name it msudomsudo?

--- VMEXE/userpasshostssh.sh
+++ VMEXE/userpasshostssh.sh
@@ -17,7 +17,7 @@ sleep 5
                                                                                  
 sudo hostnamectl set-hostname $1                                                    
 sed -i "s/msudo/$1/g" /etc/hosts                                                    
-sed -i "s/msudo/$1/g" /etc/hostname                                                 
+echo "$1" > /etc/hostname
 cat /etc/hosts                                                                      
 cat /etc/hostname                                                                   
 sudo service hostname stop                                                          
