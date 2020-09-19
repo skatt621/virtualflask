20c480d96f184922625881318c6b7aa86d4d7da2
clouduser <clouduser@notrealemail.com>
2020-09-19 12:02:06 -0400

    less stuffgit status! repl'd

--- VMEXE/cpvm.sh
+++ VMEXE/cpvm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# $1 = name, $2 = type, $3 = base, $4 = port, $5 = uuid, $6 = username, $7 = password, $8 = mode, $9 = mem
+# $1 = name, $2 = base, $3 = port, $4 = uuid, $5 = username, $6 = password, $7 = mode, $8 = mem
 set -x
 
 # ==============================================
 # ==============================================
 
-VBoxManage createvm --basefolder "{{{DIREC}}}/VMS" --name $1 --uuid=$5 --ostype $2 --register
-VBoxManage modifyvm $1 --cpus 1 --memory $9 --vram 12
+VBoxManage createvm --basefolder "{{{DIREC}}}/VMS" --name $1 --uuid=$4 --ostype $2 --register
+VBoxManage modifyvm $1 --cpus 1 --memory $8 --vram 12
 echo "================="
 echo "VM CREATED"
 
@@ -15,29 +15,29 @@ echo "====DETAILS====" > {{{DIREC}}}/VMS/$1/details.txt
 echo "STATE: PROVISIONING" >> {{{DIREC}}}/VMS/$1/details.txt
 echo "VM name: $1" >> {{{DIREC}}}/VMS/$1/details.txt
 echo "OS type: $2" >> {{{DIREC}}}/VMS/$1/details.txt
-echo "VM base type: $3" >> {{{DIREC}}}/VMS/$1/details.txt
-echo "VRDE port: $4" >> {{{DIREC}}}/VMS/$1/details.txt
-echo "UUID: $5" >> {{{DIREC}}}/VMS/$1/details.txt
-echo "Username: $6" >> {{{DIREC}}}/VMS/$1/details.txt
-echo "Password: $7" >> {{{DIREC}}}/VMS/$1/details.txt
-echo "Connection mode: $8" >> {{{DIREC}}}/VMS/$1/details.txt
+echo "VM base type: $2" >> {{{DIREC}}}/VMS/$1/details.txt
+echo "VRDE port: $3" >> {{{DIREC}}}/VMS/$1/details.txt
+echo "UUID: $4" >> {{{DIREC}}}/VMS/$1/details.txt
+echo "Username: $5" >> {{{DIREC}}}/VMS/$1/details.txt
+echo "Password: $6" >> {{{DIREC}}}/VMS/$1/details.txt
+echo "Connection mode: $7" >> {{{DIREC}}}/VMS/$1/details.txt
 echo "================="
 echo "DETAILS RECORDED"
 
-VBoxManage clonevdi "{{{DIREC}}}/BASE/$3/$3.vdi" "{{{DIREC}}}/VMS/$1/$1.vdi"
+VBoxManage clonevdi "{{{DIREC}}}/BASE/$2/$2.vdi" "{{{DIREC}}}/VMS/$1/$1.vdi"
 VBoxManage storagectl $1 --name "SATA Controller" --add sata --bootable on
 VBoxManage storageattach $1 --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "{{{DIREC}}}/VMS/$1/$1.vdi"
 echo "================="
 echo "VDI CLONED AND ATTACHED"
 
-if [ "$8" == "RDP" ]
+if [ "$7" == "RDP" ]
 then
-    VBoxManage modifyvm $1 --vrde on --vrdeport $4 --vrdeaddress {{{ADDRESS}}}
+    VBoxManage modifyvm $1 --vrde on --vrdeport $3 --vrdeaddress {{{ADDRESS}}}
     echo "================="
     echo "VRDE TURNED ON"
 fi
 
-if [ "$8" == "SSH" ]
+if [ "$7" == "SSH" ]
 then
     VBoxManage modifyvm "$1" --natpf1 "hostssh,tcp,,$port,,22"
     echo "================="
@@ -48,14 +48,14 @@ VBoxManage startvm $1 --type headless
 
 if [ $2 = "Ubuntu_64" ]
 then
-    {{{DIREC}}}/VMEXE/sshvm.sh $1 $6 $7
+    {{{DIREC}}}/VMEXE/sshvm.sh $1 $5 $6
     sed -i "s/STATE: PROVISIONING/STATE: RUNNING/g" {{{DIREC}}}/VMS/$1/details.txt
 fi
 
 if [ $2 = "Windows10_64" ]
 then
     # Not implemented
-    {{{DIREC}}}/VMEXE/sshvm-win.sh $1 $6 $7
+    {{{DIREC}}}/VMEXE/sshvm-win.sh $1 $5 $6
 fi
 echo "================="
 echo "VM PROVISIONED AND DETAILS ADDED"
--- VMEXE/cvm.sh
+++ VMEXE/cvm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# $1 = name, $2 = type, $3 = isopath, $4 = port, $5 = uuid $6 = mode, $7 = hddrive in megabytes, $8 = memory in megabytes
+# $1=name, $2=isopath, $3=port, $4=uuid, $5=mode, $6=hddrive in megabytes, $7=memory in megabytes
 set -x
 
 # ==============================================
 # ==============================================
 
-VBoxManage createvm --basefolder "{{{DIREC}}}/VMS" --name $1 --uuid=$5 --ostype $2 --register
-VBoxManage modifyvm $1 --cpus 1 --memory $8 --vram 12
+VBoxManage createvm --basefolder "{{{DIREC}}}/VMS" --name $1 --uuid=$4 --ostype $2 --register
+VBoxManage modifyvm $1 --cpus 1 --memory $7 --vram 12
 echo "================="
 echo "VM CREATED"
 
@@ -15,43 +15,43 @@ echo "====DETAILS====" > {{{DIREC}}}/VMS/$1/details.txt
 echo "STATE: PROVISIONING" >> {{{DIREC}}}/VMS/$1/details.txt
 echo "VM name: $1" >> {{{DIREC}}}/VMS/$1/details.txt
 echo "OS type: $2" >> {{{DIREC}}}/VMS/$1/details.txt
-echo "ISO file: $3" >> {{{DIREC}}}/VMS/$1/details.txt
-echo "VRDE port: $4" >> {{{DIREC}}}/VMS/$1/details.txt
-echo "UUID: $5" >> {{{DIREC}}}/VMS/$1/details.txt
-echo "Connection mode: $6" >> {{{DIREC}}}/VMS/$1/details.txt
+echo "ISO file: $2" >> {{{DIREC}}}/VMS/$1/details.txt
+echo "VRDE port: $3" >> {{{DIREC}}}/VMS/$1/details.txt
+echo "UUID: $4" >> {{{DIREC}}}/VMS/$1/details.txt
+echo "Connection mode: $5" >> {{{DIREC}}}/VMS/$1/details.txt
 echo "================="
 echo "DETAILS RECORDED"
 
-VBoxManage createhd --filename "{{{DIREC}}}/VMS/$1/$1.vdi" --size $7 --variant Standard
+VBoxManage createhd --filename "{{{DIREC}}}/VMS/$1/$1.vdi" --size $6 --variant Standard
 VBoxManage storagectl $1 --name "SATA Controller" --add sata --bootable on
 VBoxManage storageattach $1 --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "{{{DIREC}}}/VMS/$1/$1.vdi"
 echo "================="
 echo "VDI CREATED AND ATTACHED"
 
 VBoxManage storagectl $1 --name "IDE Controller" --add ide
-VBoxManage storageattach $1 --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "{{{DIREC}}}/ISOS/$3"
+VBoxManage storageattach $1 --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "{{{DIREC}}}/ISOS/$2"
 echo "================="
 echo "IDE CONTROLLER CREATED AND ISO ATTACHED"
 
-VBoxManage modifyvm $1 --vrde on --vrdeport $4
+VBoxManage modifyvm $1 --vrde on --vrdeport $3
 echo "================="
 echo "VRDE TURNED ON"
 
-if [ "$6" == "RDP" ]
+if [ "$5" == "RDP" ]
 then
-    VBoxManage modifyvm $1 --vrde on --vrdeport $4 --vrdeaddress {{{ADDRESS}}}
+    VBoxManage modifyvm $1 --vrde on --vrdeport $3 --vrdeaddress {{{ADDRESS}}}
     echo "================="
     echo "VRDE TURNED ON"
 fi
 
-if [ "$6" == "SSH" ]
+if [ "$5" == "SSH" ]
 then
-    VBoxManage modifyvm "$1" --natpf1 "hostssh,tcp,,$4,,22"
+    VBoxManage modifyvm "$1" --natpf1 "hostssh,tcp,,$3,,22"
     echo "================="
     echo "SSH TURNED ON"
 fi
 
-VBoxManage startvm $5 --type headless
+VBoxManage startvm $4 --type headless
 echo "================="
 echo "VM STARTED"
 
