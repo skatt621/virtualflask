3ac66bfea7d27b2a64d6a3e3761ffe7327145399
clouduser <clouduser@notrealemail.com>
2020-09-23 10:15:05 -0400

    added delete flag

--- VMEXE/dvm.sh
+++ VMEXE/dvm.sh
@@ -4,7 +4,7 @@ set -x
 for var in "$@"
 do
     VBoxManage controlvm $var poweroff
-    VBoxManage unregistervm $var
+    VBoxManage unregistervm $var --delete
     rm -r "{{{DIREC}}}/VMS/$var"
     rm -r "{{{DIREC}}}/WEBAPP/static/$var"
     rm "{{{DIREC}}}/WEBAPP/error_$var.log"
